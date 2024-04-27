bf3a952e039ceefe7e652875f5a66644d208706e
Nikhil Gaikwad <162660405+Lthecoders@users.noreply.github.com>
2024-04-27 12:47:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,113 @@
+# input("what is your name")
+# myName = input("What's you name? ")
+# myAge = input("How old are you? ")
+# print("Gee, that's REALLY OLD")
+# replit = input("Do you like Replit? ")
+# print("OF COURSE YOU DO!")
+
+# print()
+# print("So, you are")
+# print(myName)
+# print("and are the ripe old age of")
+# print(myAge)
+# print("and clearly think that Replit is")
+# print(replit)
+
+# myLunch = input("What are you having for lunch? ")
+# print("Hmm, it sounds like you really should just order")
+# print(myLunch)
+# print("as soon as possible!")
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+"----------day 2 challenge---------------"
+
+# print("Getting to know you!")
+# print()
+# name = input("what is your name? : ")
+# favourite_food = input("what is your favourite food ? : ")
+# favourite_music = input("what is your favourite music ? : ")
+# liveWhere = input("where do you live ?")
+
+# print()
+# print("your are ")
+# print(name)
+# print("Your're probably hungry for")
+# print(favourite_food)
+# print("and you're definitely getting your groove on to")
+# print(favourite_music)
+# print("living in the amazing")
+# print(liveWhere)
+"""
+output of the challenge
+Getting to know you!
+
+what is your name? : nikhil
+what is your favourite food ? : rice
+what is your favourite music ? : pop
+where do you live ?India
+
+your are 
+nikhil
+Your're probably hungry for
+rice
+and you're definitely getting your groove on to
+pop
+living in the amazing
+India
+"""
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+###########################################################################################
+
+# print("Definitely not a Phishing Scam")
+# print()
+# myName = input("Your Name")
+# print("Thanks for logging in")
+# print(myName)
+# cardNumber = input("What is your credit card number?")
+# print(cardNumber)
+# print("Thanks, I definitely wont put")
+# print("cardNumber")
+# print("into Amazon and order anything weird")
+# print()
+# print("Promise")
+# maidenName = input("What is your Mother's maiden name? ")
+# print()
+# print("That's cool, I just wanted to know that")
+# print(maidenName)
+# # print(
+#     "was your Mum's maiden name. Not because the bank requested it or anything, honest."
+# )
+"""
+output
+Definitely not a Phishing Scam
+
+Your NameNikhil
+Thanks for logging in
+Nikhil
+What is your credit card number?34xx 12xx xx12 1xx4
+34xx 12xx xx12 1xx4
+Thanks, I definitely wont put
+cardNumber
+into Amazon and order anything weird
+
+Promise
+What is your Mother's maiden name? mummy
+
+That's cool, I just wanted to know that
+mummy
+was your Mum's maiden name. Not because the bank requested it or anything, honest.
+"""
+
+
+print("wow the world")
+input("wow the world")
